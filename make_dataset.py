8caf159f6f96d2659a5faaee714c9d2291085ce6
B Atmaja <b-atmaja@aist.go.jp>
2021-10-27 17:32:45 +0900

    change dir name

--- make_dataset.py
+++ make_dataset.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 
 data = []
 
-for path in tqdm(Path("/home/aad13432ni/github/ser_greek/content/data/aesdd").glob("**/*.wav")):
+for path in tqdm(Path("/data/aesdd").glob("**/*.wav")):
     name = str(path).split('/')[-1].split('.')[0]
     label = str(path).split('/')[-2]
     
@@ -53,7 +53,7 @@ df.groupby("emotion").count()[["path"]]
 """For training purposes, we need to split data into train test sets; in this 
 specific example, we break with a `20%` rate for the test set."""
 
-save_path = "/home/aad13432ni/github/ser_greek/content/data/"
+save_path = "content/data/"
 
 train_df, test_df = train_test_split(df, 
     test_size=0.2, random_state=101, stratify=df["emotion"])
